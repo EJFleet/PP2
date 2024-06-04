c832a5c34a5a4e7e5f50ea8912c5f26554ed6135
Emma Fleetwood <emmafleetwood@gmail.com>
2024-06-04 21:12:07 +0000

    Add comments to html and css; add skeleton js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,12 @@
 document.addEventListener("DOMContentLoaded", function() {
 
+    let questions = [
+
+    ];
+    
+
     let buttons = document.getElementsByTagName("button");
+
     for (let button of buttons) {
         button.addEventListener("click", function() {
             if (this.getAttribute("class") === "run-game-button"){
@@ -11,4 +17,28 @@ document.addEventListener("DOMContentLoaded", function() {
             }
         });
     }
+
 });
+
+
+/**
+ * The game loop, called when the "Let's Go" or "Try Again" buttons are clicked
+ */
+
+function runGame(){
+
+}
+
+/**
+ * Displays questions and four answer options
+ */
+function displayQuestion(){
+
+}
+
+function checkAnswer(){
+
+    ifAnswerCorrect();
+
+    
+}
